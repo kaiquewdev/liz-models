7c121a6b4cc0a34e4472b4b724511a2552f5ace4
Kaique da Silva <kaique.silva.dev@gmail.com>
2017-11-09 18:15:21 -0200

    Removed a wrong label for the output

--- travel-price.js
+++ travel-price.js
@@ -12,7 +12,7 @@ const ndarray = require('ndarray');
 //const dim = ndarray([(Math.max(10,(Math.round(Math.random()*100))))]);
 const args = process.argv.slice(2);
 const dim = ndarray(args.map(v => parseFloat(v,10)));
-console.log(`Input data: ${dim.data} meters`);
+console.log(`Input data: ${dim.data}`);
 model
   .ready()
   .then(() => {
